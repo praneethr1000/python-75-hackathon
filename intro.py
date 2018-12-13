7140662501fddfed5d26a06ef3565b493dacb39a
P. PRANEETH REDDY <praneethr1000@gmail.com>
2018-12-13 21:19:08 +0530

    Add files via upload
    Introduction involving print statements, type conversion, commenting, arithmetic

---
+++ intro.py
@@ -0,0 +1,36 @@
+# Introduction
+# -->'#' is used for commenting single lines
+'''
+  These are used for commenting multiple lines
+'''
+# Normal print statements
+'''
+    Print statement is used for print
+    Various types of print statements are as follows:
+'''
+print("Praneeth Reddy") #Praneeth Reddy
+
+print("Praneeth"+"Reddy") #PraneethReddy
+
+name="Praneeth"
+print(name)     #Praneeth
+
+print(15+10)      #25
+
+x=15+10
+print(x)
+
+'''
+Follow BODMAS rule for arithmetic expressions
+If a statement or equation involves it is defaulted as float
+'''
+y=15-10*0/5
+print(y) #15.0
+
+'''
+    Type Conversion
+    int() is used to convert expression to int, similarly other datatypes can also be type casted
+'''
+
+z="10"
+print(int(z)+10) #20
